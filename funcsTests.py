af500a40ad265a875a6d4952ad19eba3370972eb
RussellCaletena <RussellCaletena@gmail.com>
2017-06-01 21:20:45 -0700

    Setup funcsTests.py for evaluating test cases

--- funcsTests.py
+++ funcsTests.py
@@ -3,3 +3,22 @@
 # Names: Russell Caletena
 # Instructor: S. Einakian
 # Section: 5
+
+# ======================================
+# Import libraries
+# ======================================
+import unittest
+from fileMatchingFuncs import *
+
+class TestCases (unittest.TestCase):
+    # Add test functions here
+    def test_1():
+        pass
+    def test_2():
+        pass
+    def test_3():
+        pass
+
+# Run the unit tests.
+if __name__ == '__main__':
+    unittest.main()
