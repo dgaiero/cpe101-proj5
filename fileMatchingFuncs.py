2890c5774c116d80abee1b12c03529e7299f11c0
RussellCaletena <RussellCaletena@gmail.com>
2017-06-01 22:37:27 -0700

    Add more lines to sort account number

--- fileMatchingFuncs.py
+++ fileMatchingFuncs.py
@@ -4,13 +4,14 @@
 # Instructor: S. Einakian
 # Section: 5
 
+# I don't know how to access 'oldMaster.dat' in given directory so I have
+# created a temporary 'oldMaster.dat' file in the repository
 from given import *
 
 def sortAccountNumber(fileName):
     fin = open(fileName)
     masterList = []
-    unsortedAccountNumberList = []
-    sortedAccountNunmberList = []
+    sortedAccountNumberList = []
     i = 0
 
     for line in fin:
@@ -23,11 +24,27 @@ def sortAccountNumber(fileName):
     while i < len(masterList):
         #print (masterList[i][0])
         unsortedAccountNumber = int(masterList[i][0])
-        unsortedAccountNumberList.append(unsortedAccountNumber)
+        sortedAccountNumberList.append(unsortedAccountNumber)
         i += 1
-    unsortedAccountNumberList.sort()
-    print ()
-    print (unsortedAccountNumberList)
+    sortedAccountNumberList.sort()
+    print()
+    print (sortedAccountNumberList)
+
+    j = 0
+    finalList = []
+    for item in sortedAccountNumberList:
+        print (item)
+
+        #int(masterList[j][0])
+        if item == (int(masterList[j][0])):
+            print ('True')
+            finalList.append(masterList[j])
+        else:
+            continue
+        j += 1
+    print (finalList)
+
+
     #print (sortedList)
 
 
