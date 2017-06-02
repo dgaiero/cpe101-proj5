d328d8e5988d9f2fbb244374b0f342026dffee9f
RussellCaletena <RussellCaletena@gmail.com>
2017-06-01 22:07:00 -0700

    Added temporary oldMaster.dat file to repository (instead of in given file) to access and added sorting accountNumber function to fileMatchingFuncs.py

--- fileMatchingFuncs.py
+++ fileMatchingFuncs.py
@@ -1,7 +1,34 @@
 # Project 5
-# 
+#
 # Names: Russell Caletena and Dominic Gaiero
 # Instructor: S. Einakian
 # Section: 5
 
+from given import *
 
+def sortAccountNumber(fileName):
+    fin = open(fileName)
+    masterList = []
+    unsortedAccountNumberList = []
+    sortedAccountNunmberList = []
+    i = 0
+
+    for line in fin:
+        words = line.split()
+        print (words)
+        masterList.append(words)
+    print ()
+    print (masterList)
+
+    while i < len(masterList):
+        #print (masterList[i][0])
+        unsortedAccountNumber = int(masterList[i][0])
+        unsortedAccountNumberList.append(unsortedAccountNumber)
+        i += 1
+    unsortedAccountNumberList.sort()
+    print ()
+    print (unsortedAccountNumberList)
+    #print (sortedList)
+
+
+sortAccountNumber('oldMaster.dat')
