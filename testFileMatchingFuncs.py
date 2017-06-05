11d6d5fb24a9620494472d1efcd3b78aa3e43aa8
dgaiero <dpgaiero@gmail.com>
2017-06-04 21:17:02 -0700

    Removed extra print statements

--- testFileMatchingFuncs.py
+++ testFileMatchingFuncs.py
@@ -11,7 +11,6 @@ def getTextFileInfo(fileName):
     for line in fin:
         words = line.split()
         textFileInfoList.append(words)
-    print(textFileInfoList)  # Added just to see what list prints
     return textFileInfoList
 
 
@@ -23,16 +22,12 @@ def sortAccountNumber(textFileInfoList):
         sortedAccountNumberList.append(unsortedAccountNumber)
         i += 1
     sortedAccountNumberList.sort()
-    print()
-    print(sortedAccountNumberList)  # Added just to see what list prints
     return sortedAccountNumberList
 
 
 def sortTextFileInfo(textFileInfoList):
     sortTextFileInfoList = textFileInfoList
     sortTextFileInfoList.sort(key=lambda a: a[0])
-    print()
-    print(sortTextFileInfoList)  # Added just to see what list prints
     return sortTextFileInfoList
 
 
@@ -40,9 +35,6 @@ def createOldMaster(sortTextFileInfoList):
     fout = open('sorted_oldMaster.dat', 'w')
     k = 0
     while k in range(len(sortTextFileInfoList)):
-        #tempString = ''
-        #tempString += '  '.join(finalList[k])
-        #print (tempString)
         tempString = '{:4} {:6} {:11} {:10} {:15} {:12}'.format(
             sortTextFileInfoList[k][0],
             sortTextFileInfoList[k][1],
@@ -59,9 +51,6 @@ def createNewMaster(sortTextFileInfoList):
     fout = open('newMaster.dat', 'w')
     k = 0
     while k in range(len(sortTextFileInfoList[1])):
-        #tempString = ''
-        #tempString += '  '.join(finalList[k])
-        #print (tempString)
         tempString = '{:4} {:6} {:11} {:10} {:15} {:12}'.format(
             sortTextFileInfoList[1][k][0],
             sortTextFileInfoList[1][k][1],
@@ -86,69 +75,9 @@ def matchTransaction(old_sorted_list, transactionFile, accountNumbers):
                 add_balance = float(transactionFile[j][1])
                 old_sorted_list[i][3] = "{:.2f}".format(
                     old_balance + add_balance)
-                # print (old_sorted_list[i])
             else:
                 if int(transactionFile[j][0]) not in accountNumbers:
                     notFound.append(int(transactionFile[j][0]))
                     accountNumbers.append(int(transactionFile[j][0]))
-                    # print("Not found - {}".format(transactionFile[j][0]))
-        # print("\n")
-    # print("\n\n\n\n")
-    # print(old_sorted_list)
-    # print("\n\n\n\n")
     masterData = [notFound, old_sorted_list]
     return(masterData)
-
-    '''
-def addTransaction(FileName, inputList):
-    fin = open(FileName)
-    transactionList = []
-    for line in fin:
-        words = line.split()
-        transactionList.append(words)
-    #print (transactionList)
-
-    i = 0
-    tempL = []
-    j = 0
-
-    transactionList.sort()
-    print (transactionList)
-
-    floatTransactionList = []
-    for item in transactionList:
-        floatTransactionList.append(float(item[1]))
-    print (floatTransactionList)
-
-    while j < len(floatTransactionList):
-        if item == '27.14':
-            print ('True')
-        j += 1
-    '''
-    '''
-        for item2 in item:
-            item2 = float(item2)
-            print (item2)
-            tempL.append(item2)
-    print (tempL)
-    '''
-    '''
-    while i < len(transactionList):
-        floatTransactionList = []
-        for item in transactionList[i]:
-            #item = float(item)
-            floatTransactionList.append(float(item))
-            #j += 1
-        i += 1
-    '''
-
-    '''
-    print (floatTransactionList)
-    #print (type(floatTransactionList[0]))
-    '''
-    '''
-        unsortedTransaction = (transactionList[i][0])
-        #transactionList.append(unsortedTransaction)
-        i += 1
-    '''
-#addTransaction('transaction.dat', x)
