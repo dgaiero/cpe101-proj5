02f8cbe419d91984756adca98ab93ae67f30296f
RussellCaletena <RussellCaletena@gmail.com>
2017-06-08 21:31:18 -0700

    Add test cases for sortTextFileInfo

--- funcsTests.py
+++ funcsTests.py
@@ -67,6 +67,7 @@ class TestCases (unittest.TestCase):
         self.assertEqual(sortAccountNumber(getTextFileInfo('newIn2.dat')), [
             101, 127, 210, 245, 715, 788])
         self.assertEqual(sortAccountNumber(getTextFileInfo('newIn3.dat')), [10, 103, 111, 555, 808])
+
     def test_sortTextFileInfo(self):
         self.assertEqual(sortTextFileInfo(getTextFileInfo('oldMaster.dat')), [
             ['100', 'Alan', 'Jones', '348.17', '8053564820', 'SLO'],
@@ -78,7 +79,25 @@ class TestCases (unittest.TestCase):
             ['600', 'Nicol', 'Green', '-20.00', '8058875571', 'SLO'],
             ['700', 'Suzy', 'Green', '-14.22', '8052586912', 'SLO'],
             ['800', 'Mike', 'Rosen', '-104.58', '8051200891', 'Pismo_Beach'],
-        ])
+            ])
+
+        self.assertEqual(sortTextFileInfo(getTextFileInfo('newIn2.dat')), [
+            ['101', 'Ayssa', 'Sherman', '240.22', '8051298970', 'SLO'],
+            ['127', 'Robbin', 'Perry', '-59.37', '8051234820', 'Arroyo_Grande'],
+            ['210', 'Nolan', 'Woodbreak', '327.22', '8059855128', 'SLO'],
+            ['245', 'Jeffry', 'Smith', '15.31', '8057901000', 'Santa_Maria'],
+            ['715', 'Erick', 'Dowsy', '123.22', '8052006912', 'Atascadero'],
+            ['788', 'Laris', 'Nolan', '697.86', '805125789', 'Paso_Robles'],
+            ])
+
+        self.assertEqual(sortTextFileInfo(getTextFileInfo('newIn3.dat')), [
+            ['010', 'Jo', 'Lee', '-185.02', '8052348970', 'Santa_Maria'],
+            ['103', 'Kent', 'Lee', '1698.48', '8123464820', 'SLO'],
+            ['111', 'Shay', 'Jam', '143.02', '8051200891', 'Santa_Rose'],
+            ['555', 'Renei', 'McCart', '100.59', '8057901237', 'Santa_Rose'],
+            ['808', 'Sara', 'Grave', '-83.73', '8052586912', 'Morro_Bay'],
+            ])
+            
     def test_matchTransaction(self):
         self.assertEqual(matchTransaction(sortTextFileInfo(getTextFileInfo('oldMaster.dat')),
             getTextFileInfo('transaction.dat'),
